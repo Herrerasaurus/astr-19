3cb86468ad8b99db5fdf0b88d912d7fbec0753fc
Herrerasaurus <146883762+Herrerasaurus@users.noreply.github.com>
2023-10-03 12:54:32 -0700

    Add files via upload
    We now added a program file

---
+++ hello-world.py
@@ -0,0 +1,17 @@
+
+#This function writes out
+#Hello World
+def PrintHelloWorld():
+	print("Hello World!")
+
+
+#This function defines
+#the main function
+def main():
+	PrintHelloWorld()
+
+
+#Executes main function
+#and prints out 'Hello World'
+if __name__=="__main__":
+	main()
