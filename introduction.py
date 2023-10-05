2d5d9e85a15be872172792bdfaaef76ba78481fb
Herrerasaurus <146883762+Herrerasaurus@users.noreply.github.com>
2023-10-04 20:27:19 -0700

    Add files via upload
    An edited version of the hello-world.py file to include a small introduction of myself

---
+++ introduction.py
@@ -0,0 +1,28 @@
+
+#This function prints out
+#my name and pronouns
+def PrintIntro():
+        print("\nHi, my name is Celeste Herrera,")
+        print("my pronouns are she/her\n")
+
+
+#This function prints out
+#my favorite movie/food
+def PrintFav():
+        print("I dont think I can confidently say I have a favorite movie,")
+        print("I like too many movies to ever be able to pick a favorite.")
+        print("I however do have a favorite food which is enchiladas, It\'s")
+        print("the only food I don\'t think I will ever get tired of.\n")
+
+
+#This function defines
+#the main function
+def main():
+        PrintIntro()
+        PrintFav()
+
+
+#Executes main function
+#and prints out 'Hello World'
+if __name__=="__main__":
+	main()
