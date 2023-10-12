1956629b5cc6288885fd870ebb0eb046c95a7851
Herrerasaurus <146883762+Herrerasaurus@users.noreply.github.com>
2023-10-12 12:02:28 -0700

    Update math.py

--- math.py
+++ math.py
@@ -7,41 +7,50 @@ In each case, have the program print out the data type
 of the resulting answer.
 '''
 
-#calculates the sum of two floating point nums
-##float values
-f1 = 2.5
-f2 = 3.6
+def math():
 
-tot = f1 + f2
+  #calculates the sum of two floating point nums
+	##float values
+	f1 = 2.5
+	f2 = 3.6
 
-###print out sum and data type
-print("\nthe sum of the two floating point numbers 2.5 and 3.6:")
-print(f1, "+", f2, "=", tot)
-print(type(tot))
+	tot = f1 + f2
 
-
-#calculates the difference between two integers
-##int values
-int1 = 36
-int2 = 15
-
-tot = int1 - int2
-
-###print out diff and data type
-print("\nthe difference between the two integers 36 and 15:")
-print(int1, "-", int2, "=", tot)
-print(type(tot))
+	###print out sum and data type
+	print("\nthe sum of the two floating point numbers 2.5 and 3.6:")
+	print(f1, "+", f2, "=", tot)
+	print(type(tot))
 
 
-#calculates the product of a float and int
-##float values
-f1 = 17.3
-int1 = 8
+	#calculates the difference between two integers
+	##int values
+	int1 = 36
+	int2 = 15
 
-tot = f1 * int1
+	tot = int1 - int2
 
-###print out sum and data type
-print("\nthe product of the floating point number 17.3 and the integer 8:")
-print(f1, "*", int1, "=", tot)
-print(type(tot),"\n")
+	###print out diff and data type
+	print("\nthe difference between the two integers 36 and 15:")
+	print(int1, "-", int2, "=", tot)
+	print(type(tot))
 
+
+	#calculates the product of a float and int
+	##float values
+	f1 = 17.3
+	int1 = 8
+
+	tot = f1 * int1
+
+	###print out sum and data type
+	print("\nthe product of the floating point number 17.3 and the integer 8:")
+	print(f1, "*", int1, "=", tot)
+	print(type(tot),"\n")
+
+
+def main():
+	math()
+
+#executing the main function
+if __name__=="__main__":
+	main()
